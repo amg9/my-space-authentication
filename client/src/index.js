f63b9cb7d085b0113579ecbb171bedb8e5eac491
Audrey Garcia <audrey.marcella9@gmail.com>
2019-09-23 16:15:50 -0600

    made AuthProvider

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
+import { AuthProvider, } from './providers/AuthProvider';
 
 ReactDOM.render(
-  <App />, 
+  <AuthProvider>
+    <App />
+  </AuthProvider>, 
   document.getElementById('root')
 );
---
+++ client/src/providers/AuthProvider.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+
+const AuthContext = React.createContext();
+
+export const AuthConsumer = AuthContext.Consumer;
+
+export class AuthProvider extends React.Component {
+  state = { user: null };
+
+  handleRegister = () => {
+    debugger;
+  };
+
+  handleLogin = () => {
+    debugger;
+  };
+
+  handleLogout = () => {
+    debugger;
+  };
+
+  render() {
+    return (
+      <AuthContext.Provider value={{
+        ...this.state,
+        authenticated: this.state.user !== null,
+        handleRegister: this.handleRegister,
+        handleLogin: this.handleLogin,
+        handleLogout: this.handleLogout,
+      }}>
+        {this.props.children}
+      </AuthContext.Provider>
+    );
+  };
+};
